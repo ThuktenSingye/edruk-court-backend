15ceb402f17055629a8b330a43fc5fdaa4861138
ThuktenSingye <thuktensingye2163@gmail.com>
2025-03-06 20:27:20 +0600

    Devise and JWT Setup for authentication

--- app/controllers/api/v1/users/passwords_controller.rb
+++ app/controllers/api/v1/users/passwords_controller.rb
@@ -2,7 +2,23 @@ module Api
   module V1
     module Users
       class PasswordsController < Devise::PasswordsController
+        include RackSessionsFix
+
         respond_to :json
+
+        private
+
+        def respond_with(resource, _opts = {})
+          if resource.errors.empty?
+            render json: {
+              status: { code: 200, message: "Password reset instructions sent successfully." }
+            }, status: :ok
+          else
+            render json: {
+              status: { message: "Failed to send password reset instructions. #{resource.errors.full_messages.to_sentence}" }
+            }, status: :unprocessable_entity
+          end
+        end
       end
     end
   end
--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -2,7 +2,21 @@ module Api
   module V1
     module Users
       class RegistrationsController < Devise::RegistrationsController
+        include RackSessionsFix
+
         respond_to :json
+        private
+
+        def respond_with(current_user, _opts = {})
+          if resource.persisted?
+            render json: {
+              status: { code: 200, message: "Signed up successfully." }            }
+          else
+            render json: {
+              status: { message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}" }
+            }, status: :unprocessable_entity
+          end
+        end
       end
     end
   end
