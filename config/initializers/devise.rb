cd49b5f4d3f8cdd3e2eea594772c5c9f6e58a1e9
ThuktenSingye <thuktensingye2163@gmail.com>
2025-05-11 15:41:29 +0600

    Configured Docker file and docker compose

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,7 +313,7 @@ Devise.setup do |config|
 
   # Configure JWT for token-based authentication
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = ENV.fetch("DEVISE_JWT_SECRET_KEY")
     jwt.dispatch_requests = [ [ "POST", %r{^/api/v1/login$} ] ]
     jwt.revocation_requests = [ [ "DELETE", %r{^/api/v1/logout$} ] ]
     jwt.expiration_time = 15.minutes.to_i
